14e9f6f58a42282db405a8d30d7e536f2ec5fcd9
Ricardo Arroyo <ricardo0arroyo2@gmail.com>
2015-10-10 12:04:15 -0500

    Player Package

--- LHD-Game/src/player/Player.java
+++ LHD-Game/src/player/Player.java
@@ -1,5 +1,25 @@
 package player;
 
-public class Player {
-//Add code here
+/***
+ * An interface class that all Players must inherit.
+ * 
+ * @author Ricardo Arroyo, Christian Loera
+ *
+ */
+public interface Player {
+
+	/***
+	 * Pick up an object displayed on the screen
+	 */
+	void pickUpObject();
+	
+	/***
+	 * Move the Agent 
+	 * @param x X Coordinate
+	 * @param y Y Coordinate
+	 */
+	void move(int x, int y);
+	
+	
+	
 }
